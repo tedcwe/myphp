97ca58b9f3b769e78dce86c762bf070dfa169135
Osama Ahmed <tedcwe@gmail.com>
2024-06-26 08:07:04 +0300

    version 1

--- db_config.php
+++ db_config.php
@@ -1,8 +1,8 @@
 <?php
 $db_host = 'localhost';
-$db_user = 'id22368963_tahfeez';
+$db_user = 'tahfeez';
 $db_pass = 'Tahfeez_Shaheed@@2030';
-$db_name = 'id22368963_students';
+$db_name = 'students';
 ?>
 
 
